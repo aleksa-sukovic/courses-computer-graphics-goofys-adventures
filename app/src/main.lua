96c29475770b8872b9f03a01169a356418092aea
Aleksa Šuković <aleksa.sukovic@fleka.me>
2019-06-08 13:35:34 +0200

    Implements jumping physics

--- app/src/main.lua
+++ app/src/main.lua
@@ -12,30 +12,29 @@
 --- Global Variables
 
     -- World
-    local tileWidth  = 16
-    local tileHeight = 16
-    local world      = bump.newWorld(16)
-    local gravity    = 1000
-    local blocks     = {}
+    local TILE_WIDTH       = 16
+    local TILE_HEIGHT      = 16
+    local GRAVITY          = 1.5
+    local MAX_ACCELERATION = 10
+    local WORLD            = bump.newWorld(16)
 
     --- Player
-    playerSprite    = love.graphics.newImage('assets/gripe/gripe.run_right.png')
-    playerStartL    = 0
-    playerStartT    = 0
-    playerDimension = 32
-    playerSpeed     = 100
+    local PLAYER_SPRITE      = love.graphics.newImage('assets/gripe/gripe.run_right.png')
+    local PLAYER_DIMENSION   = 32
+    local PLAYER_SPEED       = 100
+    local PLAYER_JUMP_AMOUNT = 4
 
     --- Animations
 
         --- Player
-        local a8 = anim8.newGrid(32, 32, playerSprite:getWidth(), playerSprite:getHeight())
+        local a8 = anim8.newGrid(32, 32, PLAYER_SPRITE:getWidth(), PLAYER_SPRITE:getHeight())
 
             --- Walk Right
             playerWalkRight = anim8.newAnimation(a8('1-8', 1), 0.1)
 
             -- Walk Left
-            playerWalkleft  = anim8.newAnimation(a8('1-8', 1), 0.1)
-            playerWalkleft:flipH()
+            playerWalkLeft  = anim8.newAnimation(a8('1-8', 1), 0.1)
+            playerWalkLeft:flipH()
 
             --- Jump Right
             playerJumpRight = anim8.newAnimation(a8(4, 1), 0.1)
@@ -55,102 +54,145 @@
 
     --- Loads a map from specified file
     function loadTileMap(path, mapName)
+
         --- load map
         ALT.Loader.path = path
         map = ALT.Loader.load(mapName)
 
         --- initialize tiles
-        findSolidTiles(map)
+        initializeTiles(map)
 
-        map.drawObjects = false
+        --- spawn every object
+        spawnObjects()
     end
 
     --- Load every tile that should be solid
-    function findSolidTiles(map)
+    function initializeTiles(map)
         layer = map.layers['solid']
 
         for x, y, tile in layer:iterate() do
             local block = { 
                 l = x * 16,
                 t = y * 16,
-                w = tileWidth / 4,
-                h = tileHeight / 4
+                w = TILE_WIDTH / 4,
+                h = TILE_HEIGHT / 4
             }
 
-            blocks[#blocks + 1] = block
-            world:add(block, block.l, block.t, block.w, block.h)
+            WORLD:add(block, block.l, block.t, block.w, block.h)
         end
+    end
 
+    --- Spawns every object found in 'characters' layer
+    function spawnObjects()
         for i, obj in pairs( map('characters').objects ) do
-            if obj.type == 'player' then 
-                playerStartL = obj.x
-                playerStartT = obj.y
-
-                spawnPlayer(obj.x, obj.y)
-            end
+            if obj.type == 'player' then spawnPlayer(obj.x, obj.y) end
         end
     end
 
 --- Player Functions
 
+    --- Initializes main character with given coordinates
     function spawnPlayer(x, y)
         player = {
-            name = 'Goofy',
-            l = x,
-            t = y,
-            w = playerDimension,
-            h = playerDimension,
-            velocity = 0,
-            direction = 'right'
+            name         = 'Goofy',
+            startL       = x,
+            startT       = y - PLAYER_DIMENSION,
+            l            = x,
+            t            = y - PLAYER_DIMENSION,
+            w            = PLAYER_DIMENSION,
+            h            = PLAYER_DIMENSION,
+            velocity     = 0,
+            acceleration = 0,
+            direction    = 'right'
         }
 
-        world:add(player, x, y, player.w, player.h)
+        WORLD:add(player, player.l, player.t, player.w, player.h)
     end
 
-    function movePlayer(dt)
+    --- Updates main character position
+    function updatePlayer(dt)
+
         --- Update player position and animation
-        if love.keyboard.isDown('left') then
-            player.l, player.t = world:move(player, player.l - playerSpeed * dt, player.t)
-            playAnimation      = playerWalkleft
-            player.direction   = 'left'
-        elseif love.keyboard.isDown('right') then
-            player.l, player.t = world:move(player, player.l + playerSpeed * dt, player.t)
-            playAnimation      = playerWalkRight
-            player.direction   = 'right'
+        if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+            playAnimation = movePlayer('left', dt)
+        elseif love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+            playAnimation = movePlayer('right', dt)
         else
-            if player.direction == 'left' then
-                playAnimation = playerIdleLeft 
-            else 
-                playAnimation = playerIdleRight
-            end
+            playAnimation = idlePlayer()
         end
-        playAnimation:update(dt)
+
+        -- playAnimation:update(dt)
         
-        --- apply gravity to player
-        player.velocity = player.velocity + gravity * dt / 2
-        player.l, player.t = world:move(player, player.l, player.t + (player.velocity * dt))
+        --- apply gravity and acceleration to player if its in air
+        if playerOnGround() then
+           player.acceleration = 0
+           player.velocity     = 0 
+        else
+            player.velocity     = player.velocity + dt * GRAVITY
+            player.acceleration = player.acceleration + player.velocity
+        end
+
+        player.l, player.t, collisions, len = WORLD:move(player, player.l, player.t + player.acceleration)
+
+        --- if player is in air and collision occured, make player drop
+        if not playerOnGround() and len > 0 then
+            player.acceleration = 0
+            player.velocity     = 0
+        else
+            --- handle coin collision
+        end
 
         --- die if player falls of map
-        if (player.t > map.height * tileHeight) then Die() end
+        if (player.t > map.height * TILE_HEIGHT) then Die() end
     end
 
+    function movePlayer(direction, dt)
+        --- get direction
+        local dir =  -1
+        if direction == 'right' then dir = 1 end
 
-function love.keyreleased(k)
-    if k == 'w' then isJumping = false end
-end
+        --- move player
+        player.l, player.t = WORLD:move(player, player.l + PLAYER_SPEED * dt * dir, player.t)
+        player.direction   = direction
+
+        --- return animation
+        if player.direction == 'right' then return playerWalkRight else return playerWalkLeft end
+    end
+
+    --- Returns player 'idle' position animation
+    function idlePlayer()
+        if player.direction == 'left' then
+            return playerIdleLeft
+        else
+            return playerIdleRight
+        end
+    end
+
+    --- Determines if player is currently on ground
+    function playerOnGround()
+        goalY = player.t + player.acceleration
+
+        actualX, actualY = WORLD:check(player, player.l, goalY)
+
+        if actualY == goalY then return false else return true end
+    end
 
 function love.keypressed(k)
-    if k == 'w' then isJumping = true end
+    if k == 'w' or k == 'up' then
+        player.velocity = 0
+        player.acceleration = -PLAYER_JUMP_AMOUNT
+    end
 end
 
 function Die()
-    player.l = playerStartL
-    player.t = playerStartT
-    world:update(player, playerStartL, playerStartT)
+    player.l = player.startL
+    player.t = player.startT
+
+    WORLD:update(player, player.l, player.t)
 end
 
 function drawPlayer()
-    playAnimation:draw(playerSprite, player.l, player.t)
+    playAnimation:draw(PLAYER_SPRITE, player.l, player.t)
 end
 
 function love.load()
@@ -158,7 +200,7 @@ function love.load()
 end
 
 function love.update(dt)
-    movePlayer(dt)
+    updatePlayer(dt)
 end
 
 function love.draw()
